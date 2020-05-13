da79b182023586715dcffdf5efd0b470ceadba17
taufik m <refizcorp@gmail.com>
2020-05-14 00:52:01 +0700

    create header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import  'assets/scss/style.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import LandingPage from 'pages/LandingPage';
+
+import "assets/scss/style.scss";
+
+
 function App() {
   return (
     <div className="App">
-     Hello Travler
+     <Router>
+       <Route path="/" component={LandingPage}></Route>
+     </Router>
     </div>
   );
 }
